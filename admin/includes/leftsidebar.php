21f421a853d3dc5e78f9898634f62bb320fc1f1b
sdew021 <sdewangan021021@gmail.com>
2019-10-28 15:57:55 +0530

    made modifications for the comment part in the admin page

--- admin/includes/leftsidebar.php
+++ admin/includes/leftsidebar.php
@@ -48,13 +48,13 @@
                                     <li><a href="contactus.php">Contact us</a></li>
                                 </ul>
                             </li>
-   <li class="has_sub">
+   <!---<li class="has_sub">
                                 <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Comments </span> <span class="menu-arrow"></span></a>
                                 <ul class="list-unstyled">
                                   <li><a href="unapprove-comment.php">Waiting for Approval </a></li>
                                     <li><a href="manage-comments.php">Approved Comments</a></li>
                                 </ul>
-                            </li>   
+                            </li> --->  
 
                         </ul>
                     </div>
